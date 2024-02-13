b77a65f842b8191f5408f14fe742b93cff7df482
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-02-13 21:56:28 +0400

    Update insertionSort.java

--- insertionSort.java
+++ insertionSort.java
@@ -1,16 +1,24 @@
-public class insertionSort { 
- public static void main(String[] arg){
-  int [] arr={22, 10, 23, 26, 20, 32, 14};
-  for (int i = 1; i < arr.length; i++) {
-          int cur = arr[i];
-          int j = i - 1;
-          while(j >= 0 && cur < arr[j]) {
-            arr[j+1] = arr[j];
-             j--;
-          }
-           arr[j+1] = cur;
-  }
-  for(int l:arr)
-  System.out.println(l);
+public class insertionSort {
+  public static void main(String[] arg) {
+    int[] arr = {
+      22,
+      10,
+      23,
+      26,
+      20,
+      32,
+      14
+    };
+    for (int i = 1; i < arr.length; i++) {
+      int cur = arr[i];
+      int j = i - 1;
+      while (j >= 0 && cur < arr[j]) {
+        arr[j + 1] = arr[j];
+        j--;
+      }
+      arr[j + 1] = cur;
+    }
+    for (int l: arr)
+      System.out.println(l);
   }
 }
